b9a6dd6e842a2a86ab0e40bed7af2711237f8676
barrerasantino7 <santinobarrera1@gmail.com>
2023-11-22 15:23:20 -0300

    Modificando el nav y los detalles del producto

--- src/components/Item-producto/Producto.js
+++ src/components/Item-producto/Producto.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { pedirProducto } from "../../helper/pedirdatos";
 import { useParams } from "react-router-dom";
-
+import "./producto.css"
 export  const Producto = (props) =>{
     const [producto, setProductos] = useState(null);
 
@@ -15,21 +15,30 @@ export  const Producto = (props) =>{
     },[id])
 
     return (
-        <div>
+        <div className="detalle">
             {producto && 
 
-            <div className="tarjeta" key={producto.id}>
-                <img className="tarjeta-img" src={producto.imagen} alt={producto.nombre}/>
-        
-                <div className="tarjeta_contenido">
-                    <h3>{producto.nombre}</h3>
-                    <p>Precio: {producto.precio}</p>
-                    <p>{producto.info}</p>
+            <div className="detalle_producto" key={producto.id}>
+
+
+            <img className="producto-img" src={producto.imagen} alt={producto.nombre}/>
+
+            <div className="producto_contenido">
+
+                <div className="contenido_informacion">
+                    <h3 className="informacion-nombre">{producto.nombre}</h3>
+                    <div className="informacion_datos">
+                       <p>Modelo: {producto.año}</p>
+                       <p>{producto.km} KM</p>
+                       <p>Precio: {producto.precio}</p>
+                    </div>
+                    <p className="producto-info">{producto.info}</p>
                 </div>
         
-                <div className="tarjeta-btn">
+                <div className="contenido-btn">
                     <button className="btn" onClick={props.funcion}>Añadir a favoritos <i class="fa-solid fa-star"></i></button>
                 </div>
+            </div>
         
             </div>}
         </div>
